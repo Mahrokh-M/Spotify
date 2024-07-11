591148021e4329d1c81aa49af1f3f73b9f00e597
Mahrokh-M <mahrokhmousavii44@gmail.com>
2024-07-11 11:32:32 +0330

    database connceted

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 #include "register.h"
-
+#include <QtSql>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+        db.setDatabaseName("DRIVER={ODBC Driver 17 for SQL Server};SERVER=LOCALHOST\\SQLEXPRESS;DATABASE=Spotify;Trusted_Connection=Yes;");
+
+        if (db.open()) {
+            qDebug() << "Database connected!";}
+
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowState(Qt::WindowMaximized);
